dbf1fbbbf78b00bfc92d415658e46066c8a57b0d
puarun <palghatu@CHHE209.ohsum01.ohsu.edu>
2014-06-11 17:12:32 -0700

    Added my solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This is soln to assignment 2 by puarun
+## makeCacehmatrix will make an opbject containing get, set, 
+## get_inverse and set_inverse functions as a list
+## call em using a $ sign
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  xinv<-NULL
+  set<-function(y){
+    x<<-y
+    xinv<<-NULL
+  }
+  get <- function(){return(x)}
+  setinv <- function(xinv1){    
+    xinv <<- xinv1    
+  }
+  getinv <- function(){
+    return(xinv)  
+  }  
+  list(set=set,get=get,setinv=setinv,getinv=getinv)
 }
 
-
-## Write a short comment describing this function
+## cacheSolve will first check if you have already solved the inverse of the matrix
+## if not it will solve it, if yes it will returned the cached value
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  xinv<-x$getinv()
+  if (!is.null(xinv)){
+    return(xinv)
+  }
+  matdata<-x$get()
+  xinv<-solve(matdata)
+  x$setinv(xinv)
+  return(xinv) 
 }
